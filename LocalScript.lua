741954ef7df95a53b62499f4627f7ae491f07430
antoniscoool <121098954+antoniscoool@users.noreply.github.com>
2023-09-19 12:05:58 +0200

    Update LocalScript.lua

--- LocalScript.lua
+++ LocalScript.lua
@@ -3,20 +3,23 @@
 type Pair<T,S> = {first: T, second: S}
 
 
--- Vector3 pair that contains all the informations about the next point and normal the character is going to climb to
 local targetPoint: Vector3? = nil
 local targetNormal: Vector3? = nil
+local targetObject: Part? = nil
 
---local climbingDir: Vector3 = Vector3.one
 
--- boolean pair that is used to keep track of the current player status, e.g. if the player is climbing, some other
--- parts in the code get disabled so prevent "interventions"
 local climbing: boolean = false
 local tweening: boolean = false
 
+local tween_accumulatedPos: Vector3 = Vector3.zero
+
 local Constants = {
+	NAN = 0 / 0,
+	
 	PLAYER_WALL_OFFSET = 0.4,
 	PLAYER_HOR_MOVE_AMT = 2,
+	
+	SPHERECAST_DEFAULT_RADIUS = 1,
 	
 	LEDGE_Y_THRESHOLD = 0.1
 }
@@ -24,8 +27,6 @@ local Constants = {
 local tweenValue: CFrameValue = Instance.new("CFrameValue")
 
 
--- Shorthand writing for the different raycast parameters. Created because all the parameters follow pretty much the
--- same principle (their filter type is set to "include")
 local function createRaycastParam(...: Instance): RaycastParams
 	local r = RaycastParams.new()
 	r.FilterType = Enum.RaycastFilterType.Include
@@ -33,7 +34,6 @@ local function createRaycastParam(...: Instance): RaycastParams
 	return r
 end
 
--- ...and the corresponding map containing all the necessary raycast parameters
 local RaycastFilters = {
 	default = createRaycastParam(workspace.Ledges),
 	onlyBig = createRaycastParam(workspace.Ledges.LedgesBig),
@@ -43,7 +43,6 @@ local RaycastFilters = {
 
 
 
--- All the services needed
 local uis: UserInputService = game:GetService("UserInputService")
 local runService: RunService = game:GetService("RunService")
 local tweenService: TweenService = game:GetService("TweenService") 
@@ -51,21 +50,23 @@ local tweenService: TweenService = game:GetService("TweenService")
 
 
 
--- All the components of the player needed
 local char: Model?
 local hrp: Part
 local hum: Humanoid
+local weld: Weld
 
 
--- Gets the player and it's components. It even (or should) reassign the values after the player's death
--- (components like the character model get set to nil automatically after the player's death for some reason)
+
 local player: Player = game:GetService("Players").LocalPlayer
 player.CharacterAdded:Connect(function(c: Model) 
 	char = c
 	hrp = (char and char:WaitForChild("HumanoidRootPart")) :: Part
 	hum = (char and char:WaitForChild("Humanoid")) :: Humanoid
+	
+	weld = Instance.new("Weld", hrp)
+	weld.Name = "ClimbObjAttachment"
+	weld.Part0 = hrp
 end)
-
 repeat wait() until hrp ~= nil
 
 
@@ -89,28 +90,27 @@ do
 end
 
 
-
 -- Module containing necessary math functions
 local MathUtils = {}
 do
-
-	-- Convert a Vector3's components to it's radiant representation
+	function MathUtils.angleBetween(v1: Vector3, v2: Vector3): number
+		return math.acos(math.clamp(v1.Unit:Dot(v2.Unit), -1, 1))
+	end
+	
 	function MathUtils.vec3_rad(v: Vector3): Vector3
 		return Vector3.new(math.rad(v.X),math.rad(v.Y),math.rad(v.Z))
 	end
 	
-	-- Returns the point rotated around another point given the pivot, the rotation (given as a Vector3), and the offset vector
-	-- (if no offset vector is used, Vector3.zero gets used by default)
+	
+	
 	function MathUtils.rotatePointAroundPoint(p: Vector3, a: Vector3, off: Vector3?): Vector3
 		return (CFrame.new(p) * CFrame.Angles(math.rad(a.X),math.rad(a.Y),math.rad(a.Z)) * CFrame.new(off or Vector3.zero)).Position
 	end
-
-	-- Same as rotatePointAroundPoint, but the angle should be passed as radians
+	
 	function MathUtils.rotatePointAroundPoint_AngleRad(p: Vector3, a: Vector3, off: Vector3?): Vector3
 		return (CFrame.new(p) * CFrame.Angles(a.X,a.Y,a.Z) * CFrame.new(off or Vector3.zero)).Position
 	end
 
-	-- Same as rotatePointAroundPoint, but uses the player's HumanoidRootPart position and orientation
 	function MathUtils.rotatePointAroundPlayer(off: Vector3?): Vector3
 		return MathUtils.rotatePointAroundPoint(hrp.Position,hrp.Orientation,off)
 	end
@@ -133,7 +133,6 @@ do
 			{ ["Value"] = Utils.createTargetCFrame() } )
 		
 		t.Completed:Once(function(ps: Enum.PlaybackState)
-			--task.wait(0.5)
 			tweening = false
 		end)
 		
@@ -149,17 +148,16 @@ local Raycasts = {}
 do
 	
 	local cubes: {Part} = table.create(2, (function(): Part
-		local out = Instance.new("Part")
+		local out = Instance.new("Part", workspace)
 		out.Anchored = true
 		out.CanCollide = false
 		out.BrickColor = BrickColor.random()
-		out.Parent = workspace
 		
 		return out
 	end)()
 	)
 	
-	function Raycasts.topDownCast(off: Vector3, len: number): (Vector3, Vector3, RaycastParams)
+	function Raycasts.topDownCast(off: Vector3, len: number, y: number): (Vector3, Vector3, RaycastParams)
 		
 		return MathUtils.rotatePointAroundPlayer(off),
 			Vector3.yAxis * len,
@@ -195,6 +193,21 @@ do
 		cubes[i].CFrame = CFrame.new(o, towards_point) * CFrame.new(0, 0, -distance / 2)
 	end
 end
+type RaycastsModule = typeof(Raycasts)
+
+
+
+
+local Spherecasts = {}
+do
+	function Spherecasts.topDownCast(off: Vector3, len: number): (Vector3, number, Vector3, RaycastParams)
+		return MathUtils.rotatePointAroundPlayer(off),
+			Constants.SPHERECAST_DEFAULT_RADIUS,
+			Vector3.yAxis * len,
+			RaycastFilters.default
+	end
+end
+type SpherecastsModule = typeof(Spherecasts)
 
 
 
@@ -203,15 +216,23 @@ local ClimbingFunctions = {}
 do
 	
 	type CheckInfo = Pair<Vector3, number>
+	type CheckStruct = {y: CheckInfo, xz: CheckInfo, ortho: boolean}
+	
 	type RaycastFunc = (off: Vector3, len: number, y: number) -> (Vector3, Vector3, RaycastParams)
 	
 	local function _calc_climbXDir(): number
 		return uis:IsKeyDown(Enum.KeyCode.A) and -1 or (uis:IsKeyDown(Enum.KeyCode.D) and 1 or 0 )
 	end
 	
+	local function _calc_wedgeCFrame(): CFrame
+		-- TODO: implement sth here
+		return CFrame.identity
+	end
+	
 	local climbDir: number = 0
 	
-	local function _target_fromRays(y_info: CheckInfo, xz_info: CheckInfo, ortho: boolean, player_dir_logic: RaycastFunc): (Vector3?, Vector3?)
+	
+	local function _target_fromRays(y_info: CheckInfo, xz_info: CheckInfo, ortho: boolean, player_dir_logic: RaycastFunc): (Vector3?, Vector3?, Part?)
 		
 		local x_off: Vector3 = Vector3.new(climbDir,1,1)
 		
@@ -219,34 +240,25 @@ do
 			return (tp + xz_dir * 0.4) - Vector3.new(0,2)
 		end
 		
-		
-		local y_check: RaycastResult? = workspace:Raycast(Raycasts.topDownCast(y_info.first * x_off, y_info.second))
+		local y_check: RaycastResult? = workspace:Raycast(Raycasts.topDownCast(y_info.first * x_off, y_info.second, Constants.NAN))
 		
 		if y_check then
 			
-			local xz_check: RaycastResult? = workspace:Raycast(player_dir_logic(xz_info.first * x_off, 
+			local xz_check: RaycastResult? = workspace:Raycast(player_dir_logic(xz_info.first * x_off,
 				xz_info.second * (ortho and climbDir or 1),
 				y_check.Position.Y - Constants.LEDGE_Y_THRESHOLD))
-			
-			Raycasts.visualizeRay(
-				MathUtils.rotatePointAroundPlayer(xz_info.first),
-				hrp.CFrame.RightVector * xz_info.second,
-				1
-			)
 			
 			if xz_check then
 				
 				local out_tp: Vector3 = Vector3.new(xz_check.Position.X, y_check.Position.Y, xz_check.Position.Z)
 				local out_tn: Vector3 = Vector3.new(0, math.atan2(xz_check.Normal.X, xz_check.Normal.Z), 0)
 				
-				return __adjustForPlayerModel(out_tp, xz_check.Normal), out_tn
+				return __adjustForPlayerModel(out_tp, xz_check.Normal), out_tn, xz_check.Instance
 			end
-			return nil, nil
+			return nil, nil, nil
 		end
-		return nil, nil
+		return nil, nil, nil
 	end
-	
-	type CheckStruct = {y: CheckInfo, xz: CheckInfo, ortho: boolean}
 	
 	local checkInfoLookUp: {[string]: CheckStruct} = {
 		
@@ -254,10 +266,6 @@ do
 			y = 	{ first = Vector3.new(0,7,-0.9), 	second = -6 }, 
 			xz = 	{ first = Vector3.zero, 			second = 1  },
 			ortho = false
-		},
-		
-		["vertical1"] = {
-			
 		},
 		
 		["sideways1"] = {
@@ -284,7 +292,7 @@ do
 		
 		local checkInfo: CheckStruct = checkInfoLookUp[key]
 		
-		targetPoint, targetNormal = _target_fromRays(checkInfo.y, checkInfo.xz, checkInfo.ortho, checkInfo.ortho and Raycasts.playerOrthoCast or Raycasts.playerDirCast)
+		targetPoint, targetNormal, targetObject = _target_fromRays(checkInfo.y, checkInfo.xz, checkInfo.ortho, checkInfo.ortho and Raycasts.playerOrthoCast or Raycasts.playerDirCast)
 		
 		if targetPoint and targetNormal then
 			if extra_f then extra_f() end
@@ -321,7 +329,6 @@ do
 		
 		climbDir = _calc_climbXDir()
 		
-		--_updateClimbingStuff("sideways1",0.2)
 		for i=1, 3, 1 do
 			if _updateClimbingStuff("sideways"..i, 0.2) then
 				break
@@ -332,6 +339,8 @@ do
 	function ClimbingFunctions.moveVertically()
 		
 		climbDir = _calc_climbXDir()
+		
+		
 	end
 end
 
@@ -350,26 +359,22 @@ uis.InputBegan:Connect(function(inp: InputObject, gpe: boolean)
 	
 end)
 
-
 runService.PreSimulation:Connect(function(dt: number)
-	if climbing then
-		hrp.CFrame = tweenValue.Value
-	end
-end)
-
-runService.PostSimulation:Connect(function(dt: number)
 	
-end)
-
-runService.Heartbeat:Connect(function(dt: number)
-	if climbing and not tweening then
+	if climbing then
 		
-		if uis:IsKeyDown(Enum.KeyCode.W) or
-			uis:IsKeyDown(Enum.KeyCode.S) or
-			uis:IsKeyDown(Enum.KeyCode.A) or
-			uis:IsKeyDown(Enum.KeyCode.D)
-		then
-			ClimbingFunctions.moveAlongLedge()
+		if not tweening then
+
+			if uis:IsKeyDown(Enum.KeyCode.W) or
+				uis:IsKeyDown(Enum.KeyCode.S) or
+				uis:IsKeyDown(Enum.KeyCode.A) or
+				uis:IsKeyDown(Enum.KeyCode.D)
+			then
+				ClimbingFunctions.moveAlongLedge()
+			end
 		end
+		
+		local final_cf: CFrame = tweenValue.Value
+		hrp.CFrame = final_cf
 	end
 end)
